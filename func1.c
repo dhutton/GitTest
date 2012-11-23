4542e8f1de9bfb09d9533a37d33cce96e1bf0176
Derek Hutton <derek.hutton@gmail.com>
2012-11-23 09:40:29 -0800

    First commit for this test project.

---
+++ func1.c
@@ -0,0 +1,11 @@
+
+// File: func1.c
+//
+// Contains a function to do something
+//
+
+int func1(int x, int y)
+{
+  return x + y;
+}
+
---
+++ main.c
@@ -0,0 +1,10 @@
+
+// Test application to see how git can work on Windows.
+//
+
+void main(void)
+{
+  printf("Hello, Git");
+}
+
+
