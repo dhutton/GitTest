ff0cc67472b2926e1e6410ecd481b8ba80189521
Derek Hutton <derek.hutton@gmail.com>
2012-11-25 19:56:32 -0800

    Added squaring to x in func1

--- func1.c
+++ func1.c
@@ -8,6 +8,9 @@ int func1(int x, int y)
 {
   // This first version of this function added the two arguments
   // The second version will multiply them.
+
+  // In local repo GitTest2, added the sqauring of x before the multiply.
   return x * y;
+
 }
 
